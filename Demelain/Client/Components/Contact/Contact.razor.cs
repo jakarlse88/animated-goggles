ebe10427c5c8dfa4643f02a053a55996c202b844
Jon Karlsen <karlsen.jonarild@gmail.com>
2020-02-16 15:34:43 +0100

    Contact

---
+++ Demelain/Client/Components/Contact/Contact.razor.cs
@@ -0,0 +1,28 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using Demelain.Client.Models;
+using Microsoft.AspNetCore.Components;
+
+namespace Demelain.Client.Components.Contact
+{
+    public class ContactBase : ComponentBase
+    {
+        // ReSharper disable once InconsistentNaming
+        protected readonly ContactFormInputModel _contactFormInput = new ContactFormInputModel();
+
+        [Inject] private HttpClient HttpClient { get; set; }
+
+        protected async Task HandleValidSubmit()
+        {
+            var contactInfo = new ContactFormInputModel()
+            {
+                Name = _contactFormInput.Name,
+                Subject = _contactFormInput.Subject,
+                Email = _contactFormInput.Email,
+                Message = _contactFormInput.Message
+            };
+
+            await HttpClient.PostJsonAsync("https://localhost:5002/api/contact", contactInfo);
+        }
+    }
+}
